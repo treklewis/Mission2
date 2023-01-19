12fcb15a57f645e20be14858bec9a9de429483fb
treklewis <trek.lewis@gmail.com>
2023-01-19 00:04:33 -0700

    FINAL

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -17,11 +17,11 @@ namespace Mission2
             //initialize array for roll totals for each number
             float[] rollTotals = new float[12];
 
-            //get number of rolls from user and convert to integer
+            //get number of rolls from user and convert to integer and output simulator info
             Console.WriteLine("Welcome to the dice rolling simulator!\n\nEnter the number of rolls you'd like to simulate (ex. 24): ");
             int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nSIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\nTotal number of rolls = " + inputNumber + ".\n");
 
-            Console.WriteLine("SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\nTotal number of rolls = " + inputNumber);
             //roll 2 dice
             for (int i = 0; i < inputNumber; i++)
             {
@@ -33,7 +33,7 @@ namespace Mission2
                 rollTotals[rollSum]++;
             }
        
-            //print all roll totals
+            //print all roll totals and percentages
             for (int i = 0; i < 11; i++)
             {
                 //find roll total percentage
@@ -50,7 +50,7 @@ namespace Mission2
                 Console.WriteLine((i + 2) + ": " + rollTotals[i] + " " + asterisks);
 
             }
-              
+            Console.WriteLine("\nThank you for using my dice rolling simulator, I hope it was easy to grade :-)");
         }
     }
 }
