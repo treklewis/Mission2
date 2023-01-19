67dd178d6177935333afc2bdbebb4eb281556f3a
treklewis <trek.lewis@gmail.com>
2023-01-18 22:54:13 -0700

    Add project files.

---
+++ Mission2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize random number generator
+            Random r = new Random();
+
+            //initialize array for roll totals for each number
+            int[] rollTotals = new int[6];
+
+            //roll die
+            for (int i = 0; i < 100; i++)
+            {
+                int roll = r.Next(6);
+
+                rollTotals[roll]++;
+            }
+       
+            //print all roll totals
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(rollTotals[i]);
+            }
+              
+        }
+    }
+}
